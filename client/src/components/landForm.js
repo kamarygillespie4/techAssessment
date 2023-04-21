54cce5add35d2fb93db4bf290ab5692615693b40
Kamary <gillespiekamary@gmail.com>
2023-04-21 08:45:07 -0600

    added pages and components in the front end

---
+++ client/src/components/landForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const landForm = () => {
+  return <div>landForm</div>;
+};
+
+export default landForm;
---
+++ client/src/components/ownerForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ownerForm = () => {
+  return <div>ownerForm</div>;
+};
+
+export default ownerForm;
