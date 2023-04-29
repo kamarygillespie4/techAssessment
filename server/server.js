55da92d9a5c1648a6025e5cde172221ad0424f0c
Kamary <gillespiekamary@gmail.com>
2023-04-29 08:24:17 -0600

    test

--- server/server.js
+++ server/server.js
@@ -10,10 +10,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://venerable-queijadas-a65013.netlify.app/"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Content-Type", "application/json");
 
