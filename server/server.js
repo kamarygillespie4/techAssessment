c6f8bbff8fb13b712af9801f99379184143e66dc
Kamary <gillespiekamary@gmail.com>
2023-04-29 08:06:13 -0600

    test

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path");
-const httpProxy = require("http-proxy");
+const db = require("./config/connection");
+const routes = require("./routes");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const proxy = httpProxy.createProxyServer();
-
 app.use(
-  express.urlencoded({
-    extended: true,
+  cors({
+    origin: "https://venerable-queijadas-a65013.netlify.app",
   })
 );
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,23 +22,13 @@ app.use((req, res, next) => {
   next();
 });
 
-// Proxy all requests to the API server
-app.all("/api/*", (req, res) => {
-  proxy.web(req, res, {
-    target: "https://obscure-bayou-28121.herokuapp.com",
-  });
-});
-
-// Serve your static assets
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-// Serve the index.html file for all other routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
